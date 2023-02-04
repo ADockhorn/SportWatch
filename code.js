a0342c111cbe496d20db94fd3757854476973324
Alexander Dockhorn <dockhorn.alexander@gmail.com>
2023-02-04 10:59:55 +0100

    added comments

--- code.js
+++ code.js
@@ -1,6 +1,7 @@
 var Layout = require("Layout");
 var Storage = require("Storage");
 
+/* // just to remember how layouts work
 var layout = new Layout( {
   type:"v", c: [
     {type:"txt", font:"6x8:2", label:"A Test", id:"label"},
@@ -16,36 +17,38 @@ function setLabel(x) {
   layout.label.label = x;
   layout.render();
 }
+*/
 
-
+// clear screen
 g.clear();
 
-//g.drawImage(img,10,10);
+//--- draw main screen
+// draw images
 g.drawImage(Storage.read("farmer.img"), 0, 1); // farmer
 g.drawImage(Storage.read("field.img"), 90, 15);  // field
 g.drawImage(Storage.read("stats2.img"), 9, 95); // stats
 g.drawImage(Storage.read("farm.img"), 88, 96);   // market
 
+// draw labels
 g.setFont("6x8").drawString("Contracts", 13, 75);
 g.setFont("6x8").drawString("Farm", 110, 75);
 g.setFont("6x8").drawString("Stats", 25, 165);
 g.setFont("6x8").drawString("Shop", 110, 165);
 
+// draw rectangles to indicate button areas
 g.drawRect(5, 3, 75, 83);
 g.drawRect(85, 3, 155, 83);
 g.drawRect(5, 93, 75, 173);
 g.drawRect(85, 93, 155, 173);
-//layout.render();
 
-
+// model touches
 Bangle.on('touch', function(button, xy) {
   if (xy.x > 5 && xy.x < 75 && xy.y > 3 && xy.y < 83) {
     g.clear();
     g.setFont("6x8").drawString("Contracts", 10, 10);
     g.drawImage(Storage.read("otto.img"), 9, 95); // otto
     g.drawImage(Storage.read("otto.img"), 109, 95); // otto
-    g.drawImage(Storage.read("cat.img"), 190, 95); // cat
-    
+    g.drawImage(Storage.read("cat.img"), 190, 95); // cat    
 
     Bangle.on('drag', function(event) { 
       g.clear();
@@ -56,25 +59,21 @@ Bangle.on('touch', function(button, xy) {
 
     }
      );
-                  
-    
   }
-  
+
   if (xy.x > 85 && xy.x < 155 && xy.y > 3 && xy.y < 83) {
     g.clear();
     g.setFont("6x8").drawString("Farm", 10, 10);
     g.drawImage(Storage.read("oldman.img"), 9, 95); // oldman
   }
-  
+
   if (xy.x > 5 && xy.x < 75 && xy.y > 93 && xy.y < 173) {
     g.clear();
-g.setFont("6x8").drawString("Stats", 10, 10);
-
+    g.setFont("6x8").drawString("Stats", 10, 10);
   }
-  
+
   if (xy.x > 85 && xy.x < 155 && xy.y > 93 && xy.y < 173) {
     g.clear();
     g.setFont("6x8").drawString("Shop", 10, 10);
-
   }
 });
